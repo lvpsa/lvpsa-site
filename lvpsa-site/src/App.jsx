fb10550ea88358344632f58fa4bda8158ab608d1
lvpsa <liguevpsa@gmail.com>
2026-06-01 15:50:53 -0400

    Update App.jsx

--- lvpsa-site/src/App.jsx
+++ lvpsa-site/src/App.jsx
@@ -1328,9 +1328,12 @@ const envoyerCommande = () => {
     to_email: commande.courriel,
   };
   
-fetch("TON_URL_APPS_SCRIPT_ICI", {
+fetch("https://script.google.com/macros/s/AKfycbzTGtjahqUxVwnvx8x3bboSXE7z694gA0Q-3_v8CYpXJ15_hraQgucMqpM0WkMN89ET/exec", {
   method: "POST",
   mode: "no-cors",
+  headers: {
+    "Content-Type": "text/plain;charset=utf-8",
+  },
   body: JSON.stringify({
     nom: commande.nom,
     courriel: commande.courriel,
