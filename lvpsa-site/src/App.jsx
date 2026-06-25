91946507ebd05007500bad265111b56eea2cb7bb
lvpsa <liguevpsa@gmail.com>
2026-06-24 23:16:10 -0400

    Update App.jsx

--- lvpsa-site/src/App.jsx
+++ lvpsa-site/src/App.jsx
@@ -4454,17 +4454,17 @@ const seriesCompetitifFiltres = seriesCompetitif.filter(matchConcerneEquipe);
 />
           </div>
 
-          <div className="mt-16 grid gap-8 lg:grid-cols-2">
-           <TableHoraire
-  titre="Séries récréatives"
-  matchs={seriesRecreatifFiltres}
-/>
+          <div className="mt-16 flex flex-col gap-8">
+  <TableHoraire
+    titre="Séries récréatives"
+    matchs={seriesRecreatifFiltres}
+  />
 
-<TableHoraire
-  titre="Séries compétitives"
-  matchs={seriesCompetitifFiltres}
-/>
-          </div>
+  <TableHoraire
+    titre="Séries compétitives"
+    matchs={seriesCompetitifFiltres}
+  />
+</div>
         </>
       )}
 
