afd9bc72ae7b59ac54f9407ed6d9d8dc7596942f
Michael Theroux <liguevpsa@gmail.com>
2026-08-01 22:38:10 -0400

    Increase height of a div from 20 to 24

--- lvpsa-site/src/pages/AccueilV2.jsx
+++ lvpsa-site/src/pages/AccueilV2.jsx
@@ -217,7 +217,7 @@ export default function AccueilV2() {
       </div>
 
       <HeaderLVPSA />
-      <div className="h-20" />
+      <div className="h-24" />
 
       <main className="relative z-10">
 
